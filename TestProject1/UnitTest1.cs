ff4b31781188755ee9e8bf33a28be43a2c2caadb
wojtek <losiutrol@gmail.com>
2023-05-24 16:21:58 +0200

    test.jednostkowy

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -5,11 +5,7 @@ namespace TestProject1
 {
     public class UnitTest1
     {
-        private readonly KalkulatorDelty _program;
-        public UnitTest1()
-        {
-            _program = new KalkulatorDelty();
-        }
+       
         [Fact]
         public void TesObliczDelte_Zwraca_2_pierwiastki()
         {
@@ -18,7 +14,7 @@ namespace TestProject1
             double a = 2;
             double b = 3;
             // act
-            var lol = KalkulatorDelty.ObliczPierwiastki(delta,a,b);
+            var lol = KalkulatorDelty.ObliczPierwiastki(delta, a, b);
             // assert
             Assert.NotNull(lol);
             Assert.Equal("Pierwiastki równania kwadratowego to: x1 = 0,5, x2 = -2", lol);
@@ -27,11 +23,11 @@ namespace TestProject1
         public void ObliczDelte_Zwraca_1_pierwiastek()
         {
             double delta = 0;
-                double a = 1;
-                double b = -4;
+            double a = 1;
+            double b = -4;
             var lol2 = KalkulatorDelty.ObliczPierwiastki(delta, a, b);
             Assert.NotNull(lol2);
-            Assert.Equal("Równanie kwadratowe ma jeden pierwiastek: x = -2", lol2);
+            Assert.Equal("Równanie kwadratowe ma jeden pierwiastek: x = 2", lol2);
         }
         [Fact]
         public void ObliczDelte_Brak_Pierwiastow()
@@ -44,7 +40,7 @@ namespace TestProject1
             Assert.Equal("Równanie nie ma pierwiastków rzeczywistych.", lol3);
         }
         [Theory]
-        [InlineData(0,1,4)]
+        [InlineData(0, 1, 4)]
         public void Zwraca_2(double delta, double a, double b)
         {
             var lol4 = KalkulatorDelty.ObliczPierwiastki(delta, a, b);
